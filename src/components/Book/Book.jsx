effe7764c58bb5e0ffcbb62d1e0d9e9a45315014
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-11-16 09:11:49 +0600

    first commit

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,62 @@
+const Book = ({ book }) => {
+  const { image, bookName, author, tags, category, bookId } = book;
+  return (
+    <Link to={`/books/${bookId}`}>
+      <div className="card bg-base-300 w-96 shadow-xl p-6">
+        <figure className="bg-blue-200 py-8 rounded-2xl">
+          <img src={image} className="h-[166px]" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <div className="flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <div className="badge badge-outline bg-green-100 text-green-600 font-semibold p-3">
+                {tag}
+              </div>
+            ))}
+          </div>
+          <h2 className="card-title">
+            {bookName}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>By: {author}</p>
+          {/* divider */}
+          <div className="my-3 border-t-2 border-dashed border-gray-500"></div>
+          <div className="card-actions justify-between">
+            <div className="badge badge-outline">{category}</div>
+            {/* rating */}
+            <div className="rating">
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+                defaultChecked
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+              <input
+                type="radio"
+                name="rating-2"
+                className="mask mask-star-2 bg-orange-400"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Book;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
+
+const Home = () => {
+  return (
+    <div className="mx-auto">
+      <Banner />
+      <Books />
+    </div>
+  );
+};
+
+export default Home;
